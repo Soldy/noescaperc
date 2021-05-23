d559dc49dd9ff9aaa0c9015353267337cbd95b76
Soldy <4786022+Soldy@users.noreply.github.com>
2021-05-23 23:46:08 +0100

    style update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 
-exports.noescape = function escapeHtml(intext) {
-    let map = {
+exports.noescape = function () {
+    this.getHtml = function(in_text){
+        return _getHtml (in_text);
+    };
+    const _map_html = {
       "&":"&amp;",
       "<":"&lt;",
       ">":"&gt;",
       "'":"&apos;",
       '"':"&quot;"
     };
-    if(typeof intext !== "undefined") 
+    const _getHtml = function(in_text){
+        if(typeof in_text === "undefined")
+            return undefined;
         return intext.toString().replace(
-            /[&<>"']/g, 
-            function(m){
-                return map[m]; 
-            }
-        );
-    return undefined;
+             /[&<>"']/g, 
+            function(in_char){
+                return _map_html[in_char]; 
+             }
+       );
+    };
 }
 
 
