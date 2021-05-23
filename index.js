ce2d7d71cc6b03bad1d866131525fc67b1fd97ff
Soldy <4786022+Soldy@users.noreply.github.com>
2021-05-23 23:52:49 +0100

    0.0.2
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
+/*
+ *  @Soldy\noescaperc\2021.02.07\GPL3
+ */
+'use strict';
 
-exports.noescape = function () {
+const noEscapeBase = function () {
     this.getHtml = function(in_text){
         return _getHtml (in_text);
     };
     const _map_html = {
-      "&":"&amp;",
-      "<":"&lt;",
-      ">":"&gt;",
-      "'":"&apos;",
-      '"':"&quot;"
+        '&':'&amp;',
+        '<':'&lt;',
+        '>':'&gt;',
+        '\'':'&apos;',
+        '"':'&quot;'
     };
     const _getHtml = function(in_text){
-        if(typeof in_text === "undefined")
+        if(typeof in_text === 'undefined')
             return undefined;
-        return intext.toString().replace(
-             /[&<>"']/g, 
+        return in_text.toString().replace(
+            /[&<>"']/g, 
             function(in_char){
                 return _map_html[in_char]; 
-             }
-       );
+            }
+        );
     };
-}
-
+};
+exports.base = noEscapeBase;
 
